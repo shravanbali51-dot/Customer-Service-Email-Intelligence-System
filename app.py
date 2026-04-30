9b70e7e581a4f5b618753bb5e61639f9685ef6ea
shravanbali51-dot <shravanbali51@gmail.com>
2026-04-30 21:25:13 +0530

    final deploy fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from textblob import TextBlob
 import pickle
+import os
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -250,4 +251,5 @@ def stats():
                            low_priority=low_priority)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
